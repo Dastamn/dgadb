e893f60913e12ddffe5c336ba257e16c20b9e6ff
Tobias Möller <tobias@moller.se>
2025-08-28 14:26:18 +0200

    try to fix syntax error

--- scripts/bitcoin.py
+++ scripts/bitcoin.py
@@ -44,26 +44,26 @@ for t in ("alpha", "otc"):
         df_edge_features_num = df.select(["edge_id", "weight"]).rename(
             {"weight": "f0"}).unpivot(["f0"], index="edge_id", variable_name="feature_id")
         
-        """#SLADE FORMATTING 
-        df_overall_state = df.group_by("tgt").agg(pl.sum("weight")).with_columns(pl.when(pl.col(
-            "weight") >= 0).then(0).otherwise(1).alias("overall_state")).drop("weight").rename({"tgt": "node_id"})
-        overall_state = set(df_overall_state.filter(
-            pl.col("overall_state") == 1)["node_id"].to_list())
-        current_dynamic_abnormal = set()
-
-        edge_label_list = []
-        for row in df.iter_rows(named=True):
-            if row["weight"] < 0:
-                # update the current dynamic state if needed
-                if row["tgt"] in overall_state:
-                    current_dynamic_abnormal.add(row["tgt"])
-            if row["src"] in current_dynamic_abnormal:
-                edge_label_list.append(1)
-            else:
-                edge_label_list.append(0)
-        df_edge_labels = df.select(["edge_id"]).with_columns(
-            pl.Series("label", edge_label_list)
-        ) """
+        #SLADE FORMATTING 
+        #df_overall_state = df.group_by("tgt").agg(pl.sum("weight")).with_columns(pl.when(pl.col(
+        #    "weight") >= 0).then(0).otherwise(1).alias("overall_state")).drop("weight").rename({"tgt": "node_id"})
+        #overall_state = set(df_overall_state.filter(
+        #    pl.col("overall_state") == 1)["node_id"].to_list())
+        #current_dynamic_abnormal = set()
+        #
+        #edge_label_list = []
+        #for row in df.iter_rows(named=True):
+        #    if row["weight"] < 0:
+        #       # update the current dynamic state if needed
+        #       if row["tgt"] in overall_state:
+        #            current_dynamic_abnormal.add(row["tgt"])
+        #   if row["src"] in current_dynamic_abnormal:
+        #       edge_label_list.append(1)
+        #   else:
+        #       edge_label_list.append(0)
+        #df_edge_labels = df.select(["edge_id"]).with_columns(
+        #    pl.Series("label", edge_label_list)
+        #) """
 
     file_path_edges = os.path.join(dataset_dir, "edges.parquet")
     file_path_edge_features_num = os.path.join(
@@ -71,7 +71,7 @@ for t in ("alpha", "otc"):
     file_path_edge_labels = os.path.join(
         dataset_dir, "edge_labels.parquet")
     df_edges.write_parquet(file_path_edges)
-    """ df_edge_features_num.write_parquet(file_path_edge_features_num)
-    df_edge_labels.write_parquet(file_path_edge_labels) """
+    #df_edge_features_num.write_parquet(file_path_edge_features_num)
+    #df_edge_labels.write_parquet(file_path_edge_labels) """
 
 print("success.")
