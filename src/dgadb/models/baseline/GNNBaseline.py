fd8f2b09ddc6489866a12745a497dceeca5b615b
Dastamn <mezhoudi.nazim@gmail.com>
2025-09-15 11:06:58 +0200

    Added gnn baselines, bad val auc

--- src/dgadb/models/baseline/GNNBaseline.py
+++ src/dgadb/models/baseline/GNNBaseline.py
@@ -1,15 +1,18 @@
 import logging
 
-from typing import Callable, Optional, Type
+from typing import Callable, Optional, Type, cast
 
 import torch
 import torch.nn.functional as F
 
 from tqdm import tqdm
 from torch.types import Device
+from torch.utils.data import DataLoader
 from torch_geometric.nn import GAT, GCN, GraphSAGE
 from torch_geometric.utils import negative_sampling
 from torch_geometric.nn import SAGEConv, GATConv, InnerProductDecoder
+from torch_geometric.loader import DataLoader, LinkNeighborLoader, NeighborLoader, LinkLoader, NodeLoader
+from torch_geometric.data import Data
 
 from ..base import BaseModel
 from src.dgadb.storage import TemporalGraph, TemporalGraphSnapshotLoader
@@ -37,11 +40,16 @@ class GNNBaseline(BaseModel):
         self.out_channels = hyperparams.get("out_channels", 128)
         self.num_layers = hyperparams.get("num_layers", 2)
         self.act = hyperparams.get("act", "ReLU")
+        self.num_neighbors = hyperparams.get("num_neighbors", 10)
         self.num_epoch = hyperparams.get("num_epoch", 100)
         self.learning_rate = hyperparams.get("learning_rate", 0.01)
         self.model_name = hyperparams.get("model", "GCN")
         self.snap_size = hyperparams.get("snap_size", 2000)
         self.print_freq = hyperparams.get("print_freq", 10)
+
+        self.epoch_evaluation_metric = epoch_evaluation_metric
+
+        self.decoder = InnerProductDecoder()
 
         self.logger.info(f"Initialized GNNBaseline: {self.model_name}.")
 
@@ -66,7 +74,6 @@ class GNNBaseline(BaseModel):
             act=str(self.act)
         )
 
-        # Move to device
         if str(self.device).startswith("cuda") and torch.cuda.device_count() > 1:
             self.logger.info(f"Using {torch.cuda.device_count()} GPUs.")
             self.model = torch.nn.DataParallel(self.model)
@@ -83,58 +90,127 @@ class GNNBaseline(BaseModel):
         assert self.model is not None
         assert self.optimizer is not None
 
-        train_snaps = TemporalGraphSnapshotLoader(
-            self.temporal_graph, window_size=self.snap_size, split="train")
-        val_snaps = TemporalGraphSnapshotLoader(
-            self.temporal_graph, window_size=self.snap_size, split="val")
+        train_mask = self.temporal_graph.train_mask
+        train_edge_index = self.temporal_graph.edge_index[:, train_mask]
 
-        train_src = self.temporal_graph.src[self.temporal_graph.train_mask]
-        train_tgt = self.temporal_graph.tgt[self.temporal_graph.train_mask]
-        train_edge_index = torch.stack([train_src, train_tgt], dim=1).T
+        val_mask = self.temporal_graph.val_mask
+        val_edge_index = self.temporal_graph.edge_index[:, val_mask]
+
+        data = Data(
+            x=self.temporal_graph.node_attr,
+            y=self.temporal_graph.node_labels,
+            edge_index=self.temporal_graph.edge_index,
+            edge_attr=self.temporal_graph.msg,
+            time=self.temporal_graph.t,
+            edge_label=self.temporal_graph.edge_labels,
+            edge_weight=self.temporal_graph.w
+        )
+
+        train_loader = LinkNeighborLoader(
+            data,
+            batch_size=self.snap_size,
+            num_neighbors=[int(self.num_neighbors)] * int(self.num_layers),
+            edge_label_index=train_edge_index,
+            edge_label=None,  # All positives
+            neg_sampling_ratio=1.0,
+            shuffle=False
+        )
+
+        val_loader = LinkNeighborLoader(
+            data,
+            num_neighbors=[int(self.num_neighbors)] * int(self.num_layers),
+            edge_label_index=val_edge_index,
+            edge_label=self.temporal_graph.edge_labels[val_mask],
+            shuffle=False
+        )
 
         for epoch in tqdm(range(int(self.num_epoch))):
             self.model.train()
             self.optimizer.zero_grad()
 
-            for train_snap in train_snaps:
-                curr = train_snap.current
-                assert curr.node_attr is not None
-                cum = train_snap.cumulative
+            for batch in train_loader:
                 z = self.model(
-                    x=cum.node_attr,
-                    edge_index=cum.edge_index,
-                    edge_attr=curr.msg,
-                    edge_weight=curr.w
+                    x=batch.x,
+                    edge_index=batch.edge_index,
+                    edge_attr=batch.edge_attr,
+                    edge_weight=batch.edge_weight
                 )
-                z_src = z[curr.src]
-                z_tgt = z[curr.tgt]
-                pos_logits = (z_src * z_tgt).sum(dim=1)
 
-                neg_edge_index = negative_sampling(
-                    edge_index=train_edge_index,
-                    num_nodes=self.temporal_graph.num_nodes,
-                    num_neg_samples=curr.num_edges,
-                )
-                neg_z_src = z[neg_edge_index[0]]
-                neg_z_tgt = z[neg_edge_index[1]]
-                neg_logits = (neg_z_src * neg_z_tgt).sum(dim=1)
+                src, tgt = batch.edge_label_index
+                train_preds = F.sigmoid((z[src] * z[tgt]).sum(dim=-1))
+                # train_preds = self.decoder(
+                #     z, batch.edge_label_index, sigmoid=True)
+                loss = F.binary_cross_entropy(train_preds, batch.edge_label)
 
-                loss = F.binary_cross_entropy_with_logits(
-                    torch.cat([pos_logits, neg_logits]),
-                    torch.cat([torch.zeros_like(pos_logits),
-                              torch.ones_like(neg_logits)])
-                )
                 loss.backward()
                 self.optimizer.step()
 
-                self.inference()
-                break
+            if ((epoch + 1) % self.print_freq == 0) or runnable is not None:
+                val_preds, val_labels = self.test(val_loader)
+                auc = self.epoch_evaluation_metric(
+                    val_labels, val_preds.detach())
 
-            # if (epoch + 1) % self.print_freq == 0:
-            #     self.inference()
+                self.logger.info("VAL AUC: " + str(auc))
 
-            break
+                if runnable is not None:
+                    runnable(auc, self, epoch)
 
-    def inference(self):
+    def test(self, loader: Optional[TemporalGraphSnapshotLoader | LinkLoader | NodeLoader] = None) -> tuple[torch.Tensor, torch.Tensor]:
         self._ensure_setup()
-        print("inference")
+        assert self.temporal_graph is not None
+        assert self.model is not None
+
+        self.model.eval()
+        all_preds, all_labels = [], []
+
+        if loader is None:
+            # Do 'test' inference
+            test_mask = self.temporal_graph.test_mask
+            test_edge_index = self.temporal_graph.edge_index[:, test_mask]
+
+            data = Data(
+                x=self.temporal_graph.node_attr,
+                y=self.temporal_graph.node_labels,
+                edge_index=self.temporal_graph.edge_index,
+                edge_attr=self.temporal_graph.msg,
+                time=self.temporal_graph.t,
+                edge_label=self.temporal_graph.edge_labels,
+                edge_weight=self.temporal_graph.w
+            )
+
+            loader = LinkNeighborLoader(
+                data,
+                num_neighbors=[int(self.num_neighbors)] * int(self.num_layers),
+                edge_label_index=test_edge_index,
+                edge_label=self.temporal_graph.edge_labels[test_mask],
+                shuffle=False
+            )
+
+        elif not isinstance(loader, LinkNeighborLoader):
+            raise RuntimeError(
+                f"'loader' should be of type LinkNeighborLoader, got: {type(loader).__name__}.")
+
+        loader = cast(LinkNeighborLoader, loader)
+
+        for batch in loader:
+            z = self.model(
+                x=batch.x,
+                edge_index=batch.edge_index,
+                edge_attr=batch.edge_attr,
+                edge_weight=batch.edge_weight
+            )
+            src, tgt = batch.edge_label_index
+            preds = F.sigmoid((z[src] * z[tgt]).sum(dim=-1))
+            # preds = self.decoder(z, batch.edge_label_index, sigmoid=True)
+            all_preds.append(preds.cpu())
+            all_labels.append(batch.edge_label.cpu())
+
+        p, l = torch.cat(all_preds), torch.cat(all_labels)
+        # acc = ((p > 0.5) == l.bool()).float().mean().item()
+        # print(acc)
+        return p, l
+        # return torch.cat(all_preds), torch.cat(all_labels)
+
+    def inference(self, split, loader=None):
+        self._ensure_setup()
+        return self.test(loader=loader if split != "test" and loader is not None else None)
