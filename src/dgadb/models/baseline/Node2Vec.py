5b5a7ce9ce366f6add57055bc86ebe3a5cbf51e1
Dastamn <mezhoudi.nazim@gmail.com>
2025-09-30 22:19:49 +0200

    Moved probs to cpu in validation

--- src/dgadb/models/baseline/Node2Vec.py
+++ src/dgadb/models/baseline/Node2Vec.py
@@ -104,7 +104,7 @@ class n2vModel:
             logger.info(
                 f"Epoch {epoch+1}/{self.num_epochs} - mean loss: {epoch_loss:.4f}")
             probs, labels = self.inference(split="val")
-            auc = self.epoch_evaluation_metric(labels, probs.detach())
+            auc = self.epoch_evaluation_metric(labels, probs.detach().cpu())
             if runnable is not None:
                 runnable(auc, self, epoch, save=False)
 
