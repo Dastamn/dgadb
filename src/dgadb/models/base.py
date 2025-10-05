2d3ffbf6c35266a74206b3577ef629c0aa8eb001
Dastamn <mezhoudi.nazim@gmail.com>
2025-10-05 13:56:25 +0200

    Added new improved base class

--- src/dgadb/models/base.py
+++ src/dgadb/models/base.py
@@ -1,3 +1,8 @@
+from sklearn.metrics import roc_auc_score
+from src.dgadb.evaluation import evaluate
+from src.dgadb.storage import TemporalGraphView
+from dataclasses import dataclass, fields
+from src.dgadb.storage import TemporalGraphSnapshot, TemporalGraphSnapshotLoader
 import logging
 import typing
 from abc import ABC, abstractmethod
@@ -6,6 +11,7 @@ import torch
 from torch_geometric.loader import LinkLoader, NodeLoader
 
 from src.dgadb.storage import TemporalGraph, TemporalGraphSnapshotLoader
+from typing import Callable, Generic, Optional, Self, TypeVar
 
 
 class BaseModel(ABC):
@@ -42,5 +48,79 @@ class BaseModel(ABC):
 
     @torch.no_grad()
     @abstractmethod
-    def test(self, loader: typing.Optional[TemporalGraphSnapshotLoader | LinkLoader | NodeLoader] = None) -> tuple[torch.Tensor, torch.Tensor]:
-        ...
+    def test(
+        self, loader: typing.Optional[TemporalGraphSnapshotLoader | LinkLoader | NodeLoader] = None
+    ) -> tuple[torch.Tensor, torch.Tensor]: ...
+
+
+@dataclass
+class BaseADModelComponents:
+    pass
+
+    def to(self, device: torch.device | str):
+        for field in fields(self):
+            attr = getattr(self, field.name)
+            if isinstance(attr, torch.nn.Module) or (isinstance(attr, torch.Tensor) and attr.is_floating_point()):
+                setattr(self, field.name, attr.to(device))
+            elif isinstance(attr, torch.optim.Optimizer):
+                for state in attr.state.values():
+                    for k, v in state.items():
+                        if isinstance(v, torch.Tensor):
+                            state[k] = v.to(device)
+        return self
+
+
+BaseADModelComponentsType = TypeVar("BaseADModelComponentsType", bound=BaseADModelComponents)
+
+
+class BaseADModel(Generic[BaseADModelComponentsType], ABC):
+    def __init__(self, device: torch.device | str = "cpu") -> None:
+        self.device = device
+        self._components: Optional[BaseADModelComponentsType] = None
+
+    @property
+    def components(self) -> BaseADModelComponentsType:
+        if self._components is None:
+            raise RuntimeError("Model is not initialized. Call `setup(data)` first.")
+        return self._components
+
+    def run_inference(self, loader: TemporalGraphSnapshotLoader) -> tuple[torch.Tensor, torch.Tensor]:
+        all_scores = []
+        all_labels = []
+        for snapshot in loader:
+            current_graph = snapshot.current
+            edge_scores = self.predict(snapshot)
+            edge_labels = current_graph.edge_labels
+            all_scores.append(edge_scores)
+            all_labels.append(edge_labels)
+
+        all_labels = torch.cat(all_labels)
+        all_scores = torch.cat(all_scores)
+
+        return all_labels, all_scores
+
+    def setup(self, data: TemporalGraph, **kwargs) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def train(
+        self,
+        epochs: int,
+        train_loader: TemporalGraphSnapshotLoader,
+        val_loader: Optional[TemporalGraphSnapshotLoader] = None,
+        report_callback: Optional[Callable] = None,
+    ) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self, snapshot: TemporalGraphSnapshot) -> torch.Tensor:
+        raise NotImplementedError
+
+    @abstractmethod
+    def save(self, save_dir: str) -> None:
+        raise NotImplementedError
+
+    @classmethod
+    @abstractmethod
+    def load(cls, load_dir: str, device: torch.device | str = "cpu") -> Self:
+        raise NotImplementedError
